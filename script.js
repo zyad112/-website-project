10601290b1b1eec412a0f34c3934cfb3620cfa7f
zyad112 <zyadhassana7@gmail.com>
2024-12-11 18:52:04 +0200

    Add files via upload
    Signed-off-by: zyad112 <zyadhassana7@gmail.com>

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
+
 function handleLogin(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const recaptchaResponse = grecaptcha.getResponse();
-
-    if (!recaptchaResponse) {
-        alert("يرجى التحقق من أنك لست روبوتًا!");
-        return;
-    }
 
     if (email && password) {
-        if (email === "test@gmail.com" && password === "123456") {
-            alert("تم تسجيل الدخول بنجاح!");
-            window.location.href = "dashboard.html";
-        } else {
-            alert("البريد الإلكتروني أو كلمة المرور غير صحيحة.");
-        }
+        alert(`تم تسجيل الدخول للبريد: ${email}`);
     } else {
-        alert("يرجى إدخال جميع البيانات!");
+        alert('يرجى إدخال البيانات كاملة!');
     }
 }
